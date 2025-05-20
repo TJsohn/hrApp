cb86fdeb2442057d8d622e10a79bf31ce5a8f873
TJsohn <sohntaejeong@gmail.com>
2025-05-20 14:34:53 +0300

    add step5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink, Link} from 'react-router';
 import './Header.css';
 
 const Header = ({name}) => {
     return (
         <header>
-            <div>
+            <div className="logo">
                 <Link to="/">
                 <h2>{name}</h2>
                 </Link>
             </div>
             <nav>
                 <ul>
+                    <NavLink to="/">Person list</NavLink>
+                    <NavLink to="/add">Add employee</NavLink>
                     <NavLink to="/about">About</NavLink>
-                    <NavLink to="/">Person List</NavLink>
-                    <NavLink to="/add">Add Employee</NavLink>
                 </ul>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
